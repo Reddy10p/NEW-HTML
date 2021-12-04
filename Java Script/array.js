823250fc8ca11ed2e3544262f5873d5b7484e71e
Reddy10p <varun8088@outlook.com>
2021-12-04 22:06:56 +0530

    commit

---
+++ Java Script/array.js
@@ -0,0 +1,3 @@
+let emp = ["Sujatha", "Vanaja", "Varun", "Reddy", "Pulla"];
+console.log(emp[0]);
+console.log(emp[3]);
---
+++ Sublime Text 3/JSSUBLIMETEXT.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> JavaScript </title>
+<script type="text/javascript">
+document.write("JavaScript Programming");
+</script>
+<head>
+<body> 
+Hello welcome Varun Reddy
+</body>
+</html>
