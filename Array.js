0ee94f9a5e9716811e94427fd2b6dee04319e19e
Reddy10p <varun8088@outlook.com>
2021-12-04 23:02:38 +0530

    commit

---
+++ Array.js
@@ -0,0 +1,6 @@
+let fruits = ['mango', 'Banana', 'Apple', 'potato', 'Carrot'];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
--- Java Script/array.js
+++
@@ -1,3 +0,0 @@
-let emp = ["Sujatha", "Vanaja", "Varun", "Reddy", "Pulla"];
-console.log(emp[0]);
-console.log(emp[3]);
